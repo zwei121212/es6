ef8a6862e1171897b5d94f5d21f518296fb21013
Geno <ko131313@163.com>
2018-01-12 14:52:35 +0800

    complete scripts.js

--- task/pages.js
+++ task/pages.js
@@ -1,2 +1,10 @@
 const gulp = require('gulp');
 const gulpif  = require('gulp-if');
+const livereload = require(gulp - livereload);
+const args = require('./util/args.js');
+
+gulp.task('pages',()=>{
+    return gulp.src('app/**/*.ejs')
+        .pipe(gulp.dest('server'))
+        .pipe(gulpif(args.watch,livereload()))
+})
